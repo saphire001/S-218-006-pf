67d949ed694d4c48a0af78d052c84613def0ecbf
saanikajoshi <spj29@njit.edu>
2022-05-11 17:27:31 -0400

    add new test

--- tests/click_test.py
+++ tests/click_test.py
@@ -1,13 +1,10 @@
-import logging
 import os
 
 from click.testing import CliRunner
 
-from app import create_database
+from app.cli import create_database, create_log_folder
 
 runner = CliRunner()
-
-
 
 
 def test_create_database():
@@ -17,4 +14,16 @@ def test_create_database():
     # set the name of the apps log folder to logs
     dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
-    assert os.path.exists(dbdir) == True
+    assert os.path.exists(dbdir)
+
+
+def test_create_log_folder():
+    response = runner.invoke(create_log_folder)
+    assert response.exit_code == 0
+    root = os.path.dirname(os.path.abspath(__file__))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, '../logs')
+    # make a directory if it doesn't exist
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    assert os.path.exists(logdir)
