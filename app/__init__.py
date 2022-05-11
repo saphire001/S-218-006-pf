e3d2b8f8f57975b17298cb682960f2b99eb742e8
saanikajoshi <spj29@njit.edu>
2022-05-10 20:35:57 -0400

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 """A simple flask web app"""
-import logging
 import os
+import logging
 
 import flask_login
 from flask import Flask
@@ -8,7 +8,7 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf.csrf import CSRFProtect
 from flask_cors import CORS
 from flask_mail import Mail
-
+ 
 from app.auth import auth
 from app.auth import auth
 from app.cli import create_database, create_log_folder
@@ -19,6 +19,7 @@ from app.db.models import User
 from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
+from app.songs import songs
 
 mail = Mail()
 
@@ -51,6 +52,7 @@ def create_app():
     # these load functionality without a web interface
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
+    app.register_blueprint(songs)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
