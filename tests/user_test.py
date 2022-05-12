fd48af431330f2016327a0369baaa5d355aa31f6
saanikajoshi <spj29@njit.edu>
2022-05-11 23:30:16 -0400

    tests

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,37 +1,35 @@
-#import logging
 
-#from app import db
-#from app.db.models import User, Transactions
+from app import db
+from app.db.models import User
 from app.auth.forms import *
 
-# def test_adding_user(application):
-#     log = logging.getLogger("myApp")
-#     with application.app_context():
-#         assert db.session.query(User).count() == 0
-#         assert db.session.query(Transactions).count() == 0
-#
-#         user = User('keith@webizly.com', 'testtest', True)
-#         db.session.add(user)
-#
-#         user = User.query.filter_by(email='keith@webizly.com').first()
-#         log.info(user)
-#
-#         assert user.email == 'keith@webizly.com'
-#
-#         user.transactions = [Transactions(12343, 'Credit'), Transactions(1243, 'Debit')]
-#         db.session.commit()
-#
-#         assert db.session.query(Transactions).count() == 2
-#
-#         transaction1 = Transactions.query.filter_by(amount = '12343')
-#         assert transaction1.amount == '12343'
-#
-#         transaction1.amount = '12543'
-#         db.session.commit()
-#
-#         transaction2 = Transactions.query.filter_by(amount = '12543')
-#         assert transaction2.amount == '12543'
-#
-#         db.session.delete(user)
-#         assert db.session.query(User).count() == 0
-#         assert db.session.query(Transactions).count() == 0
+
+def test_login(client):
+    with client:
+        user = User('keith@webizly.com', 'testtest', True)
+        db.session.add(user)
+        db.session.commit()
+
+        response = client.get("/login")
+        assert response.status_code == 200
+
+
+def test_register(application, client):
+    form = register_form()
+    form.email.data = "keith@webizly.com"
+    form.password.data = "testtest"
+    form.confirm.data = "testtest"
+    assert form.validate()
+
+
+def test_login_dashboard(client):
+    with client:
+        response = client.get("/dashboard")
+        assert response.status_code == 302
+
+
+def test_logout_dashboard(client):
+    db.session.query(User).count() == 0
+    with client:
+        response = client.get("/dashboard")
+        assert b"<h1>Redirecting...</h1>" in response.data
